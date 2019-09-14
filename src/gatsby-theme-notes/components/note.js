cbdd8b5abd3e5bc80035005f27a5fc07e1fed92a
Benjamin Lannon <benjamin@lannonbr.com>
2019-09-14 00:23:34 -0400

    adding some hover text

--- src/gatsby-theme-notes/components/note.js
+++ src/gatsby-theme-notes/components/note.js
@@ -1,10 +1,12 @@
-import React from 'react'
+/** @jsx jsx */
+
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
 import { Home, ArrowLeft } from 'react-feather'
 import Layout from './layout'
 import useOptions from 'gatsby-theme-notes/src/use-options'
+import { jsx } from 'theme-ui'
 
 const NavigationLink = styled(Link)`
   margin-bottom: 16px;
@@ -39,12 +41,30 @@ const WikiPage = ({
         }}
       >
         {backPath !== options.basePath && (
-          <NavigationLink to={backPath} style={{ marginRight: 20 }}>
+          <NavigationLink
+            to={backPath}
+            css={{
+              marginRight: 20,
+              borderBottom: '2px solid transparent',
+              ':hover': {
+                borderBottomColor: 'var(--blueLink)',
+              },
+            }}
+          >
             <ArrowLeft />
             Back
           </NavigationLink>
         )}
-        <NavigationLink to={options.basePath}>
+        <NavigationLink
+          to={options.basePath}
+          css={{
+            marginRight: 20,
+            borderBottom: '2px solid transparent',
+            ':hover': {
+              borderBottomColor: 'var(--blueLink)',
+            },
+          }}
+        >
           <Home />
           Notes Home
         </NavigationLink>
