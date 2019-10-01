a7d40808e2a9eff1e68ea96caf1db84fccebbe3a
Benjamin Lannon <benjamin@lannonbr.com>
2019-09-30 21:10:56 -0400

    Adding newest blogpost component on front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { graphql, Link } from 'gatsby'
 import { SplitLayout } from '../components/Containers'
 
 import SEO from '../components/Utils/seo'
 import IllustrationImg from '../components/illustrationImg'
 
 import office from '../images/office.svg'
+import { Coffee } from 'react-feather'
 
 import styled from 'styled-components'
 
@@ -36,12 +37,46 @@ const FrontpageDiv = styled.div`
   }
 `
 
-const IndexPage = () => (
+const NewBlogComponent = styled.div`
+  font-size: 20px;
+  margin-bottom: 20px;
+  border: 1px solid #d1d5da;
+  display: inline-block;
+  padding: 16px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+
+  a {
+    margin-left: 5px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+`
+
+const IndexPage = ({ data }) => (
   <>
     <SEO
       title="Home"
       keywords={[`Benjamin Lannon`, `Portfolio`, `Web Developer`, `gatsby`]}
     />
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <NewBlogComponent>
+        <Coffee style={{ marginRight: 5 }} />
+        New Blogpost:{' '}
+        <Link to={data.newestBlogpost.nodes[0].fields.slug}>
+          {data.newestBlogpost.nodes[0].frontmatter.title}
+        </Link>
+      </NewBlogComponent>
+    </div>
     <SplitLayout>
       <FrontpageDiv>
         <h1>Hi all, I'm Benjamin</h1>
@@ -61,3 +96,22 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexQuery {
+    newestBlogpost: allMdx(
+      filter: { fileAbsolutePath: { regex: "/blog/" } }
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: 1
+    ) {
+      nodes {
+        frontmatter {
+          title
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`
