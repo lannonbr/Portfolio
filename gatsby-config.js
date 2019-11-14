4d38518c475b04ad77ef984b73257871b26449e3
Benjamin Lannon <benjamin@lannonbr.com>
2019-11-13 19:49:03 -0500

    moving blog folder into content folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/content/blog`,
       },
     },
     {
