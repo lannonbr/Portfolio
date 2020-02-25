ddac06eb34700666ba1fe76e2a3bf6d2e929b4c3
Benjamin Lannon <benjamin@lannonbr.com>
2020-02-24 22:50:54 -0500

    Cleanup index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,35 +8,6 @@ import IllustrationImg from '../components/illustrationImg'
 import office from '../images/office.svg'
 import { Coffee } from 'react-feather'
 
-import styled from 'styled-components'
-
-const FrontpageDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  h1 {
-    font-size: 70px;
-  }
-
-  p {
-    font-size: 30px;
-    line-height: 50px;
-  }
-
-  @media (max-width: 768px) {
-    h1 {
-      font-size: 30px;
-      text-align: center;
-    }
-
-    p {
-      font-size: 16px;
-      line-height: 20px;
-    }
-  }
-`
-
 const IndexPage = ({ data }) => (
   <>
     <SEO
@@ -44,30 +15,29 @@ const IndexPage = ({ data }) => (
       keywords={[`Benjamin Lannon`, `Portfolio`, `Web Developer`, `gatsby`]}
     />
     <SplitLayout>
-      <FrontpageDiv>
-        <h1>Hi all, I'm Benjamin</h1>
-        <p>
+      <div className="flex flex-col justify-center">
+        <h1 className="text-center text-4xl md:text-left md:text-6xl">
+          Hi all, I'm Benjamin
+        </h1>
+        <p className="lg:text-xl md:leading-loose">
           I'm a web developer in Upstate New York with a passion for exploration
           and continual learning.
         </p>
-        <h2 style={{ display: 'inline-flex', alignItems: 'center' }}>
-          <Coffee style={{ marginRight: 5 }} />
+        <h2 className="inline-flex items-center">
+          <Coffee className="mr-3" />
           New Posts
         </h2>
         {data.newBlogposts.nodes.map(post => {
           return (
-            <div style={{ marginBottom: 10 }}>
+            <div className="mb-2">
               <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
-              <time
-                dateTime={post.frontmatter.fullDate}
-                style={{ marginLeft: 10 }}
-              >
+              <time dateTime={post.frontmatter.fullDate} className="ml-3">
                 {post.frontmatter.date}
               </time>
             </div>
           )
         })}
-      </FrontpageDiv>
+      </div>
       <div>
         <IllustrationImg
           src={office}
