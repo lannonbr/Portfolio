e69345a9f074510c2083951cb1d629cdcdfb8cc1
Benjamin Lannon <benjamin@lannonbr.com>
2020-02-24 22:41:12 -0500

    Update illustrationImg to remove SC

--- src/components/illustrationImg.js
+++ src/components/illustrationImg.js
@@ -1,18 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const ImgWrapper = styled.img`
-  padding-left: 30px;
-  margin-right: 30px;
-
-  @media (max-width: 768px) {
-    margin-left: 0;
-    margin-right: 0;
-  }
-`
 
 const IllustrationImg = props => {
-  return <ImgWrapper {...props} />
+  return <img className="mx-0 sm:mr-8 sm:pl-8" {...props} />
 }
 
 export default IllustrationImg
