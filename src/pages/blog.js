29ad2e50d83f7ac2119a5e3710c3b4e7b8aa0e4a
Benjamin Lannon <benjamin@lannonbr.com>
2020-03-30 22:43:25 -0400

    Add curated filters to blog listing

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SEO from '../components/seo'
 import { graphql, Link } from 'gatsby'
 
@@ -11,7 +11,33 @@ const LogolessLogo = () => (
   />
 )
 
+const CuratedButton = ({ logo, name, handleClick, isSelected }) => {
+  return (
+    <button
+      className={`border border-black p-2 flex items-center rounded text-sm mr-3 hover:shadow-xl ${
+        isSelected && 'shadow-xl font-extrabold'
+      }`}
+      style={{
+        backgroundColor: 'var(--footerBackground)',
+        color: 'var(--bodyLink)',
+      }}
+      onClick={handleClick}
+    >
+      <img src={logo} alt="" className="w-5 mr-3" />
+      <span>{name}</span>
+    </button>
+  )
+}
+
 const BlogIndexPage = ({ data }) => {
+  const [category, setCategory] = useState('')
+
+  const categories = [
+    { name: 'GitHub Actions', cat: 'github-actions' },
+    { name: 'WebAssembly', cat: 'wasm' },
+    { name: 'Gatsby', cat: 'gatsby' },
+  ]
+
   return (
     <>
       <SEO
@@ -19,29 +45,55 @@ const BlogIndexPage = ({ data }) => {
         keywords={[`Benjamin Lannon`, `Portfolio`, `Web Developer`, `gatsby`]}
       />
       <h1>Blog</h1>
-      <div className="">
-        {data.allMdx.nodes.map((node) => {
-          const logo =
-            node.frontmatter.logo &&
-            data.blogLogos.nodes.filter(
-              (logo) => logo.name === node.frontmatter.logo
-            )[0].publicURL
+      <div className="flex my-8">
+        {categories.map(({ cat, name }) => (
+          <CuratedButton
+            name={name}
+            isSelected={!!(category === cat)}
+            logo={
+              data.blogLogos.nodes.filter((logo) => logo.name === cat)[0]
+                .publicURL
+            }
+            handleClick={() => {
+              if (category === '') {
+                setCategory(cat)
+              } else if (category === cat) {
+                setCategory('')
+              } else {
+                setCategory(cat)
+              }
+            }}
+          />
+        ))}
+      </div>
+      <div>
+        {data.allMdx.nodes
+          .filter((post) => {
+            if (category === '') return true
+            else return post.frontmatter.logo === category
+          })
+          .map((node) => {
+            const logo =
+              node.frontmatter.logo &&
+              data.blogLogos.nodes.filter(
+                (logo) => logo.name === node.frontmatter.logo
+              )[0].publicURL
 
-          return (
-            <Link
-              key={node.fields.slug}
-              to={node.fields.slug}
-              className="inline-block flex items-center py-2 px-3 text-xl rounded-sm transition-all duration-200 ease-in-out hover:text-purple-700 hover:bg-purple-100 mb-3"
-            >
-              {node.frontmatter.logo ? (
-                <img src={logo} alt="" className="w-6 mr-2" />
-              ) : (
-                <LogolessLogo />
-              )}
-              <span>{node.frontmatter.title}</span>
-            </Link>
-          )
-        })}
+            return (
+              <Link
+                key={node.fields.slug}
+                to={node.fields.slug}
+                className="inline-block flex items-center py-2 px-3 text-xl rounded-sm transition-all duration-200 ease-in-out hover:text-purple-700 hover:bg-purple-100 mb-3"
+              >
+                {node.frontmatter.logo ? (
+                  <img src={logo} alt="" className="w-6 mr-2" />
+                ) : (
+                  <LogolessLogo />
+                )}
+                <span>{node.frontmatter.title}</span>
+              </Link>
+            )
+          })}
       </div>
     </>
   )
