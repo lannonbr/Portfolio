5952a8e024ea11d5f230885e71d3ad73f9e67374
Benjamin Lannon <benjamin@lannonbr.com>
2020-04-11 14:52:07 -0400

    Fixed header title

--- src/components/header.js
+++ src/components/header.js
@@ -40,7 +40,16 @@ const Header = () => {
       <header className="pb-5 pt-2 md:pt-5 px-4 shadow-lg w-full max-w-7xl mx-auto grid items-center  md:shadow-none">
         <h1 className="text-2xl md:text-4xl m-0">
           <Link
-            className="h-full no-underline text-rebecca-purple dark:text-cyan"
+            className="h-full no-underline"
+            style={{
+              backgroundImage:
+                mode !== 'dark'
+                  ? 'linear-gradient(to right, #9d50bb, #6e48aa)'
+                  : 'linear-gradient(to left, #50c9c3, #96deda)',
+              background: 'rgba(0,0,0,0)  repeat scroll 0% 0% padding-box text',
+              WebkitBackgroundClip: 'text',
+              WebkitTextFillColor: 'transparent',
+            }}
             to="/"
           >
             {data.site.siteMetadata.title}
