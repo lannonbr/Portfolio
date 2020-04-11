3e3eb2e19b5f975f54f0010595ccadcdc9625258
Benjamin Lannon <benjamin@lannonbr.com>
2020-04-11 01:01:51 -0400

    Style new posts on frontpage similar to blog listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,15 @@ import IllustrationImg from '../components/illustrationImg'
 import office from '../images/office.svg'
 import { ChevronRight } from 'react-feather'
 
+const LogolessLogo = () => (
+  <div
+    className="inline-block w-6 h-6 rounded-full mr-2"
+    style={{
+      backgroundImage: 'linear-gradient( 135deg, #E2B0FF 10%, #9F44D3 100%)',
+    }}
+  />
+)
+
 const IndexPage = ({ data }) => (
   <>
     <SEO
@@ -33,9 +42,25 @@ const IndexPage = ({ data }) => (
           </Link>
         </div>
         {data.newBlogposts.nodes.map((post) => {
+          const logo =
+            post.frontmatter.logo &&
+            data.blogLogos.nodes.filter(
+              (logo) => logo.name === post.frontmatter.logo
+            )[0].publicURL
+
           return (
             <div className="mb-3">
-              <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+              <Link
+                to={post.fields.slug}
+                className="rounded py-2 px-3 flex items-center transition-all duration-200 ease-in-out hover:text-purple-700 hover:bg-purple-100 dark-hover:bg-cyan-transparent dark-hover:text-cyan-light hover:no-underline"
+              >
+                {post.frontmatter.logo ? (
+                  <img src={logo} alt="" className="w-6 mr-4" />
+                ) : (
+                  <LogolessLogo />
+                )}
+                {post.frontmatter.title}
+              </Link>
             </div>
           )
         })}
@@ -54,6 +79,13 @@ export default IndexPage
 
 export const query = graphql`
   query IndexQuery {
+    blogLogos: allFile(filter: { relativePath: { regex: "/^blog-icons/" } }) {
+      nodes {
+        name
+        publicURL
+      }
+    }
+
     newBlogposts: allMdx(
       filter: { fileAbsolutePath: { regex: "/blog/" } }
       sort: { fields: frontmatter___date, order: DESC }
@@ -62,6 +94,7 @@ export const query = graphql`
       nodes {
         frontmatter {
           title
+          logo
         }
         fields {
           slug
