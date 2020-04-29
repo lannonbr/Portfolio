6f751de13dbe6a853d2a7d3ba0ae050552823bda
Benjamin Lannon <benjamin@lannonbr.com>
2020-04-29 18:06:20 -0400

    Highlighting code blocks. Thx Prince

--- src/components/code.js
+++ src/components/code.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import rangeParser from 'parse-numeric-range'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
+
+const calculateLinesToHighlight = (meta) => {
+  const RE = /highlight={([\d,-]+)}/
+  if (RE.test(meta)) {
+    const strlineNumbers = RE.exec(meta)[1]
+    const lineNumbers = rangeParser(strlineNumbers)
+    return (index) => lineNumbers.includes(index + 1)
+  } else {
+    return () => false
+  }
+}
 
 const getParams = (name = ``) => {
   const [lang, params = ``] = name.split(` `)
@@ -16,6 +28,8 @@ const getParams = (name = ``) => {
 const Code = ({ codeString, language, className, metastring, ...props }) => {
   const [lang, { title = `` }] = getParams(className + ' ' + metastring)
 
+  const shouldHighlightLine = calculateLinesToHighlight(metastring)
+
   return (
     <Highlight
       {...defaultProps}
@@ -24,7 +38,7 @@ const Code = ({ codeString, language, className, metastring, ...props }) => {
       theme={theme}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <div className="codeBlock mb-4 shadow-xl text-sm">
+        <div className="codeBlock gatsby-highlight mb-4 shadow-xl text-sm">
           {title && (
             <div
               className={className + ' text-sm px-5 py-4'}
@@ -40,13 +54,19 @@ const Code = ({ codeString, language, className, metastring, ...props }) => {
               borderTop: title ? '1px solid #8BADC1' : 'unset',
             }}
           >
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })}></span>
-                ))}
-              </div>
-            ))}
+            {tokens.map((line, i) => {
+              const lineProps = getLineProps({ line, key: i })
+              if (shouldHighlightLine(i)) {
+                lineProps.className = `${lineProps.className} highlight-line`
+              }
+              return (
+                <div {...lineProps}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })}></span>
+                  ))}
+                </div>
+              )
+            })}
           </pre>
         </div>
       )}
