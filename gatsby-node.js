cd176652e5ee739e9afa6d94f9a378d050f8d1bc
Benjamin Lannon <benjamin@lannonbr.com>
2020-05-28 08:05:48 -0400

    One last fix for OG image work (hopefully)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
-const { createPrinterNode } = require('gatsby-plugin-printer')
 const crypto = require('crypto')
 const fs = require('fs')
 
