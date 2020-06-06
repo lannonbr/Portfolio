b903619b92744cc0136ebe8c96400e411a7d3d35
Benjamin Lannon <benjamin@lannonbr.com>
2020-06-06 17:31:26 -0400

    Renable navigation and frontpage
    Keeping BLM banner up top

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,55 @@ import office from '../images/office.svg'
 import { ChevronRight } from 'react-feather'
 
 const Banner = () => {
-  return (<div style={{
-    color: 'hsl(0, 0%, 98%)',
-    fontSize: 16,
-    lineHeight: 1.5,
-    padding: 32,
-    width: '100%',
-    backgroundColor: 'hsl(0, 0%, 4%)',
-    gridColumn: "1 / span 2"
-  }}>
-    <h1>#BlackLivesMatter</h1>
-    <p className="text-xl"><strong>I stand in solidarity</strong> with George Floyd, Natosha McDade, Yassin Mohamed, Finan H. Berhe, Sean Reed, Steven Demarco Taylor, Breonna Taylor, Ariane McCree, Terrance Franklin, Miles Hall, Darius Tarver, William Green, Samuel David Mallard, Kwame Jones, De’von Bailey, Christopher Whitfield, Anthony Hill, De’Von Bailey, Eric Logan, Jamarion Robinson, Gregory Hill Jr, JaQuavion Slaton, Ryan Twyman, Brandon Webber, Jimmy Atchison, Willie McCoy, Emantic Fitzgerald Bradford J, D’ettrick Griffin, Jemel Roberson, DeAndre Ballard, Botham Shem Jean, Robert Lawrence White, Anthony Lamar Smith, Ramarley Graham, Manuel Loggins Jr, Trayvon Martin, Wendell Allen, Kendrec McDade, Larry Jackson Jr, Jonathan Ferrell, Jordan Baker, Victor White III, Dontre Hamilton, Eric Garner, John Crawford III, Michael Brown, Ezell Ford, Dante Parker, Kajieme Powell, Laquan McDonald, Akai Gurley, Tamir Rice, Rumain Brisbon, Jerame Reid, Charly Keunang, Tony Robinson, Walter Scott, Freddie Gray, Brendon Glenn, Samuel DuBose, Christian Taylor, Jamar Clark, Mario Woods, Quintonio LeGrier, Gregory Gunn, Akiel Denkins, Alton Sterling, Philando Castile, Terrence Sterling, Terence Crutcher, Keith Lamont Scott, Alfred Olango, Jordan Edwards, Stephon Clark, Danny Ray Thomas, DeJuan Guillory, Patrick Harmon, Jonathan Hart, Maurice Granton, Julius Johnson, Jamee Johnson, Michael Dean and countless others.</p>
-    <p><strong><a href="https://blacklivesmatters.carrd.co/" style={{ color: 'hsl(0, 0%, 86%)' }}>Go here to find out how <em>you</em> can help.</a></strong></p>
-  </div>)
+  return (
+    <div
+      style={{
+        color: 'hsl(0, 0%, 98%)',
+        fontSize: 16,
+        lineHeight: 1.5,
+        padding: 32,
+        marginBottom: 16,
+        width: '100%',
+        backgroundColor: 'hsl(0, 0%, 4%)',
+        gridColumn: '1 / span 2',
+      }}
+    >
+      <h1>#BlackLivesMatter</h1>
+      <p className="text-xl">
+        <strong>I stand in solidarity</strong> with George Floyd, Natosha
+        McDade, Yassin Mohamed, Finan H. Berhe, Sean Reed, Steven Demarco
+        Taylor, Breonna Taylor, Ariane McCree, Terrance Franklin, Miles Hall,
+        Darius Tarver, William Green, Samuel David Mallard, Kwame Jones, De’von
+        Bailey, Christopher Whitfield, Anthony Hill, De’Von Bailey, Eric Logan,
+        Jamarion Robinson, Gregory Hill Jr, JaQuavion Slaton, Ryan Twyman,
+        Brandon Webber, Jimmy Atchison, Willie McCoy, Emantic Fitzgerald
+        Bradford J, D’ettrick Griffin, Jemel Roberson, DeAndre Ballard, Botham
+        Shem Jean, Robert Lawrence White, Anthony Lamar Smith, Ramarley Graham,
+        Manuel Loggins Jr, Trayvon Martin, Wendell Allen, Kendrec McDade, Larry
+        Jackson Jr, Jonathan Ferrell, Jordan Baker, Victor White III, Dontre
+        Hamilton, Eric Garner, John Crawford III, Michael Brown, Ezell Ford,
+        Dante Parker, Kajieme Powell, Laquan McDonald, Akai Gurley, Tamir Rice,
+        Rumain Brisbon, Jerame Reid, Charly Keunang, Tony Robinson, Walter
+        Scott, Freddie Gray, Brendon Glenn, Samuel DuBose, Christian Taylor,
+        Jamar Clark, Mario Woods, Quintonio LeGrier, Gregory Gunn, Akiel
+        Denkins, Alton Sterling, Philando Castile, Terrence Sterling, Terence
+        Crutcher, Keith Lamont Scott, Alfred Olango, Jordan Edwards, Stephon
+        Clark, Danny Ray Thomas, DeJuan Guillory, Patrick Harmon, Jonathan Hart,
+        Maurice Granton, Julius Johnson, Jamee Johnson, Michael Dean and
+        countless others.
+      </p>
+      <p>
+        <strong>
+          <a
+            href="https://blacklivesmatters.carrd.co/"
+            style={{ color: 'hsl(0, 0%, 86%)' }}
+          >
+            Go here to find out how <em>you</em> can help.
+          </a>
+        </strong>
+      </p>
+    </div>
+  )
 }
 
 const LogolessLogo = () => (
@@ -39,7 +75,7 @@ const IndexPage = ({ data }) => (
       keywords={[`Benjamin Lannon`, `Portfolio`, `Web Developer`, `gatsby`]}
     />
     <Banner />
-    {/* <div className="flex flex-col justify-center">
+    <div className="flex flex-col justify-center">
       <h1 className="text-center text-4xl md:text-left md:text-6xl">
         Hi all, I'm Benjamin
       </h1>
@@ -70,8 +106,8 @@ const IndexPage = ({ data }) => (
               {post.frontmatter.logo ? (
                 <img src={logo} alt="" className="w-6 mr-4" />
               ) : (
-                  <LogolessLogo />
-                )}
+                <LogolessLogo />
+              )}
               {post.frontmatter.title}
             </Link>
           </div>
@@ -80,7 +116,7 @@ const IndexPage = ({ data }) => (
     </div>
     <div className="hidden md:block">
       <IllustrationImg src={office} alt="Person sitting on desk illustration" />
-    </div> */}
+    </div>
   </section>
 )
 
