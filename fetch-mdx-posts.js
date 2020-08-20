dff3df64ada76636e1608e29875fa1b062af1f21
Benjamin Lannon <benjamin@lannonbr.com>
2020-08-20 19:31:52 -0400

    Instrumenting MDX compilation

--- fetch-mdx-posts.js
+++ fetch-mdx-posts.js
@@ -8,52 +8,62 @@ const rehypeSlug = require('rehype-slug')
 const cloudinary = require('rehype-local-image-to-cloudinary')
 const rehypePrism = require('./utils/prism-rehype-plugin')
 
+const beeline = require('honeycomb-beeline')()
+
 exports.sourceData = async ({ createPage, ...options }) => {
   console.log('sourceData')
   const files = await fs.readdir('./content/blog/')
+  beeline.addContext({ numMDXFiles: files.length })
 
-  return Promise.all(
-    files.map(async (filename) => {
-      const file = await fs.readFile(
-        `./content/blog/${filename}/index.md`,
-        'utf-8'
-      )
-      let compiledMDX
+  let data = await files.map(async (filename) => {
+    return beeline.startAsyncSpan(
+      { sourceType: 'mdx', filename },
+      async (span) => {
+        const file = await fs.readFile(
+          `./content/blog/${filename}/index.md`,
+          'utf-8'
+        )
 
-      const { data, content } = frontmatter(file)
+        let compiledMDX
 
-      try {
-        compiledMDX = await mdx(content, {
-          rehypePlugins: [
-            rehypePrism,
-            rehypeSlug,
-            [
-              cloudinary,
-              {
-                baseDir: path.join(__dirname, 'content', 'blog', filename),
-                uploadFolder: 'lannonbr.com',
-              },
+        const { data, content } = frontmatter(file)
+
+        try {
+          compiledMDX = await mdx(content, {
+            rehypePlugins: [
+              rehypePrism,
+              rehypeSlug,
+              [
+                cloudinary,
+                {
+                  baseDir: path.join(__dirname, 'content', 'blog', filename),
+                  uploadFolder: 'lannonbr.com',
+                },
+              ],
             ],
-          ],
+          })
+        } catch (e) {
+          console.log(e)
+          throw e
+        }
+
+        await createPage({
+          module: `/** @jsx mdx */
+              import {mdx} from '@mdx-js/preact';
+              ${compiledMDX}`,
+          slug: `blog/${filename}`,
+          data: { ...data, slug: filename },
         })
-      } catch (e) {
-        console.log(e)
-        throw e
-      }
 
-      await createPage({
-        module: `/** @jsx mdx */
-            import {mdx} from '@mdx-js/preact';
-            ${compiledMDX}`,
-        slug: `blog/${filename}`,
-        data: { ...data, slug: filename },
-      })
+        beeline.finishSpan(span)
 
-      // Data to be stored in `mdx-posts.json` file
-      return {
-        ...data,
-        slug: `blog/${filename}`,
+        // Data to be stored in `mdx-posts.json` file
+        return {
+          ...data,
+          slug: `blog/${filename}`,
+        }
       }
-    })
-  )
+    )
+  })
+  return data
 }
