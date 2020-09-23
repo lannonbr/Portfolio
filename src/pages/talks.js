850ce8b08af3a533c6f53f482cf244a0200e6e6f
Benjamin Lannon <benjamin@lannonbr.com>
2020-09-22 20:38:17 -0400

    Update styling for talks page

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -4,6 +4,7 @@ import SEO from '../components/seo.js'
 import Youtube from '../components/feather/youtube.js'
 import File from '../components/feather/file.js'
 import dayjs from 'dayjs'
+import { Fragment } from 'preact'
 
 const talks = [
   {
@@ -11,7 +12,8 @@ const talks = [
     date: '2020-07-09',
     event: 'Learn With Jason',
     location: 'Online',
-    recording: 'https://www.learnwithjason.dev/caching-jamstack-sites-with-github-actions'
+    recording:
+      'https://www.learnwithjason.dev/caching-jamstack-sites-with-github-actions',
   },
   {
     name: 'Getting up to speed with GitHub Actions',
@@ -68,52 +70,60 @@ const talks = [
 
 const TalksPage = () => {
   return (
-    <section className="md:grid md:grid-cols-2 md:gap-8 mb-4">
-      <SEO title="Talks" />
-      <div>
-        <h1>Talks</h1>
-        <p>
-          Through 2019, I've been starting to give presentations and workshops
-          about technologies and tools that I use or interest me.
-        </p>
+    <Fragment>
+      <section className="md:grid md:grid-cols-2 md:gap-8 mb-4">
+        <SEO title="Talks" />
+        <div>
+          <h1>Talks</h1>
+          <p>
+            Since 2019, I've been starting to give presentations and workshops
+            about technologies and tools that I use or interest me.
+          </p>
+        </div>
+        <div className="hidden md:block">
+          <TalkIllustration />
+        </div>
+      </section>
+      <section>
         <h2>List of Talks</h2>
-        {talks.map((talk) => {
-          let formattedDate = dayjs(talk.date).format("MMM D, YYYY")
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {talks.map((talk) => {
+            let formattedDate = dayjs(talk.date).format('MMM D, YYYY')
 
-          return (
-            <article className="border border-solid border-gray-400 rounded p-4 flex flex-col mb-5">
-              <header className="p-0 md:flex justify-between items-center">
-                <h3>{talk.name}</h3>
-                <p>
-                  {formattedDate} | {talk.event} | {talk.location}
-                </p>
-              </header>
-              <ul className="m-0 pl-0">
-                {talk.slides && (
-                  <li className="flex items-center">
-                    <File alt="" />
-                    <a href={talk.slides} className="ml-2">
-                      Slides
-                    </a>
-                  </li>
-                )}
-                {talk.recording && (
-                  <li className="flex items-center">
-                    <Youtube alt="YouTube Recording" />
-                    <a href={talk.recording} className="ml-2">
-                      Recording
-                    </a>
-                  </li>
-                )}
-              </ul>
-            </article>
-          )
-        })}
-      </div>
-      <div className="hidden md:block">
-        <TalkIllustration />
-      </div>
-    </section>
+            return (
+              <article className="border border-solid border-gray-400 rounded p-4 flex flex-col">
+                <header className="p-0 md:flex justify-between items-start">
+                  <h3>{talk.name}</h3>
+                  <div className="mb-4">
+                    <p className="mt-0 mb-1 md:text-right">{formattedDate}</p>
+                    <p className="mt-0 mb-1 md:text-right">{talk.event}</p>
+                    <p className="mt-0 md:text-right">{talk.location}</p>
+                  </div>
+                </header>
+                <ul className="m-0 pl-0">
+                  {talk.slides && (
+                    <li className="flex items-center">
+                      <File alt="" />
+                      <a href={talk.slides} className="ml-2">
+                        Slides
+                      </a>
+                    </li>
+                  )}
+                  {talk.recording && (
+                    <li className="flex items-center">
+                      <Youtube alt="YouTube Recording" />
+                      <a href={talk.recording} className="ml-2">
+                        Recording
+                      </a>
+                    </li>
+                  )}
+                </ul>
+              </article>
+            )
+          })}
+        </div>
+      </section>
+    </Fragment>
   )
 }
 
