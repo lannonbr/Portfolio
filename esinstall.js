c63fa69621456367f37e63e7a3472d5b0d986621
Benjamin Lannon <benjamin@lannonbr.com>
2021-04-10 14:47:03 -0400

    Update from breadbox to esinstall

---
+++ esinstall.js
@@ -0,0 +1,14 @@
+export const specs = [
+  'preact',
+  'preact/hooks',
+  '@mdx-js/preact',
+  'preact/compat',
+  'react-helmet',
+  '@headlessui/react',
+]
+
+export const options = {
+  alias: {
+    react: 'preact/compat',
+  },
+}
